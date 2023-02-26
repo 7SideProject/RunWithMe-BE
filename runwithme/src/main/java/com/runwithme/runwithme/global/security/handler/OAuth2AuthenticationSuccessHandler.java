1dc972c66db00b11e2f77b0c7f0e712922793e80
Myeongbeom Lee <mungmnb777@gmail.com>
2023-02-26 20:21:38 +0900

    feat: 로그인 성공 후 후처리 추가
    클라이언트가 요청한 위치로 리다이렉트
    Access Token 발급

---
+++ runwithme/src/main/java/com/runwithme/runwithme/global/security/handler/OAuth2AuthenticationSuccessHandler.java
@@ -0,0 +1,44 @@
+package com.runwithme.runwithme.global.security.handler;
+
+import com.runwithme.runwithme.domain.user.entity.User;
+import com.runwithme.runwithme.global.security.jwt.AuthToken;
+import com.runwithme.runwithme.global.security.jwt.AuthTokenFactory;
+import com.runwithme.runwithme.global.security.model.PrincipalDetails;
+import com.runwithme.runwithme.global.utils.CookieUtils;
+import com.runwithme.runwithme.global.utils.HeaderUtils;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import java.io.IOException;
+import java.util.Date;
+
+@Slf4j
+@RequiredArgsConstructor
+public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    @Value("${jwt.access-token.expiry}")
+    private int ACCESS_TOKEN_EXPIRY;
+
+    private static final String REDIRECT_URI = "redirect_uri";
+    private final AuthTokenFactory authTokenFactory;
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        PrincipalDetails details = (PrincipalDetails) authentication.getPrincipal();
+
+        User user = details.getUser();
+
+        AuthToken accessToken = authTokenFactory.createAuthToken(user.getEmail(), user.getNickname(), new Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRY));
+
+        HeaderUtils.setAccessToken(response, accessToken.getToken());
+
+        Cookie redirectUri = CookieUtils.getCookie(request, REDIRECT_URI).orElseThrow(IllegalArgumentException::new);
+
+        response.sendRedirect(redirectUri.getValue());
+    }
+}
--- runwithme/src/main/java/com/runwithme/runwithme/global/security/service/CustomOAuth2UserService.java
+++ runwithme/src/main/java/com/runwithme/runwithme/global/security/service/CustomOAuth2UserService.java
@@ -1,7 +1,11 @@
 package com.runwithme.runwithme.global.security.service;
 
+import com.runwithme.runwithme.domain.user.entity.User;
+import com.runwithme.runwithme.domain.user.repository.UserRepository;
+import com.runwithme.runwithme.global.security.model.PrincipalDetails;
 import com.runwithme.runwithme.global.security.provider.ProviderType;
 import com.runwithme.runwithme.global.security.provider.ProviderUserFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -9,8 +13,13 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final UserRepository userRepository;
 
     private String parseRegistrationId(OAuth2UserRequest userRequest) {
         return userRequest.getClientRegistration().getRegistrationId().toUpperCase();
@@ -22,8 +31,28 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
         OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest);
 
+        return this.process(userRequest, oAuth2User);
+    }
+
+    private OAuth2User process(OAuth2UserRequest userRequest, OAuth2User oAuth2User) {
+
         ProviderType providerType = ProviderType.valueOf(parseRegistrationId(userRequest));
 
-        return ProviderUserFactory.build(providerType, oAuth2User.getAttributes());
+        OAuth2User loginUser = ProviderUserFactory.build(providerType, oAuth2User.getAttributes());
+
+        Optional<User> checkUser = userRepository.findByEmail(loginUser.getName());
+
+        User user = checkUser.orElseGet(() -> create(loginUser));
+
+        return new PrincipalDetails(user, loginUser.getAttributes());
+    }
+
+    private User create(OAuth2User oAuth2User) {
+        User user = User.builder()
+                .email(oAuth2User.getName())
+                .point(0)
+                .build();
+
+        return userRepository.saveAndFlush(user);
     }
 }
