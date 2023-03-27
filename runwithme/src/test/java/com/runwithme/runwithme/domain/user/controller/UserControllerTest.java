309c639f7f7d40a72e92653dc231bc33ee00efbc
Myeongbeom Lee <mungmnb777@gmail.com>
2023-03-27 20:02:47 +0900

    test: 유저 프로필 수정 단위 테스트 작성

---
+++ runwithme/src/test/java/com/runwithme/runwithme/domain/user/controller/UserControllerTest.java
@@ -0,0 +1,101 @@
+package com.runwithme.runwithme.domain.user.controller;
+
+import com.runwithme.runwithme.domain.user.dto.UserProfileDto;
+import com.runwithme.runwithme.domain.user.dto.UserProfileViewDto;
+import com.runwithme.runwithme.domain.user.service.UserService;
+import com.runwithme.runwithme.utils.JacksonUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+@AutoConfigureMockMvc
+@DisplayName("유저 컨트롤러 단위 테스트")
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Nested
+    @DisplayName("[프로필 수정] 사용자는 유저 프로필(닉네임, 키, 몸무게)를 수정할 수 있습니다.")
+    class SetUserProfile {
+
+        UserProfileDto givenDto;
+        UserProfileViewDto resultDto;
+
+        @BeforeEach
+        void beforeEach() {
+            givenDto = new UserProfileDto("명범", 173, 60);
+            resultDto = UserProfileViewDto.builder()
+                    .seq(1L)
+                    .email("mungmnb777@gmail.com")
+                    .height(173)
+                    .weight(60)
+                    .nickname("명범")
+                    .point(0)
+                    .build();
+        }
+
+        @Test
+        @WithMockUser(roles = "USER")
+        @DisplayName("[성공 케이스]")
+        void success() throws Exception {
+            // given
+            BDDMockito.given(userService.setUserProfile(eq(1L), any(UserProfileDto.class))).willReturn(resultDto);
+
+            // when
+            ResultActions actions = mockMvc.perform(MockMvcRequestBuilders.put("/user/1/profile")
+                    .content(JacksonUtils.convertToJson(givenDto))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .with(SecurityMockMvcRequestPostProcessors.csrf()));
+
+            // then
+            actions.andExpect(jsonPath("$.data.seq").value(1L));
+            actions.andExpect(jsonPath("$.data.email").value("mungmnb777@gmail.com"));
+            actions.andExpect(jsonPath("$.data.height").value(173));
+            actions.andExpect(jsonPath("$.data.weight").value(60));
+            actions.andExpect(jsonPath("$.data.nickname").value("명범"));
+            actions.andExpect(jsonPath("$.data.point").value(0));
+            actions.andExpect(jsonPath("$.code").value("U002"));
+            actions.andExpect(status().isOk());
+        }
+
+        @Test
+        @WithMockUser(roles = "USER")
+        @DisplayName("[실패 케이스 1] 해당 SEQ의 유저가 없는 경우")
+        void failure() throws Exception {
+            // given
+            BDDMockito.given(userService.setUserProfile(eq(1L), any(UserProfileDto.class))).willThrow(IllegalArgumentException.class);
+
+            // when
+            ResultActions actions = mockMvc.perform(MockMvcRequestBuilders.put("/user/1/profile")
+                    .content(JacksonUtils.convertToJson(givenDto))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .with(SecurityMockMvcRequestPostProcessors.csrf()));
+
+            // then
+            actions.andExpect(jsonPath("$.code").value("U101"));
+            actions.andExpect(status().isBadRequest());
+        }
+    }
+}
