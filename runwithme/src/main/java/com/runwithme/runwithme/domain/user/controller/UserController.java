db49d25f250779f5a64a6985050d6dd240802e7b
Myeongbeom Lee <mungmnb777@gmail.com>
2023-04-11 22:12:29 +0900

    docs: 프로필 설정 문서 등록

--- runwithme/src/main/java/com/runwithme/runwithme/domain/user/controller/UserController.java
+++ runwithme/src/main/java/com/runwithme/runwithme/domain/user/controller/UserController.java
@@ -6,9 +6,14 @@ import com.runwithme.runwithme.domain.user.service.UserService;
 import com.runwithme.runwithme.global.result.ResultCode;
 import com.runwithme.runwithme.global.result.ResultResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,9 +25,13 @@ public class UserController {
 
     private final UserService userService;
 
+    @PutMapping(value = "/{userSeq}/profile", produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(summary = "프로필 설정", description = "유저 프로필을 설정하는 API입니다.")
-    @PutMapping("/{userSeq}/profile")
-    public ResponseEntity<ResultResponseDto> setUserProfile(@PathVariable Long userSeq, @RequestBody UserProfileDto dto) {
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = UserProfileViewDto.class)) }),
+            @ApiResponse(responseCode = "400", description = "잘못된 파라미터입니다.")
+    })
+    public ResponseEntity<?> setUserProfile(@PathVariable Long userSeq, @RequestBody UserProfileDto dto) {
         try {
             UserProfileViewDto userProfileViewDto = userService.setUserProfile(userSeq, dto);
             log.info("Set profile user {}", userSeq);
