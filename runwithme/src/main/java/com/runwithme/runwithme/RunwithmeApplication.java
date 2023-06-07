16e5d36c488d86cc0f3a0be10bc528ab0effd987
Myeongbeom Lee <mungmnb777@gmail.com>
2023-06-07 21:17:01 +0900

    fix: CORS로 인한 스웨거 버그 수정

--- runwithme/src/main/java/com/runwithme/runwithme/RunwithmeApplication.java
+++ runwithme/src/main/java/com/runwithme/runwithme/RunwithmeApplication.java
@@ -1,8 +1,11 @@
 package com.runwithme.runwithme;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://server.runwithme.shop")})
 @SpringBootApplication
 public class RunwithmeApplication {
 
