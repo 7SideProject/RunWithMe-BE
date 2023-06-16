3b8cc1b19ae797031f079e883de581942a8170ee
Myeongbeom Lee <mungmnb777@gmail.com>
2023-06-16 18:46:42 +0900

    refactor: GlobalExceptionHandler에 의한 예외 처리 및 예외 응답값 변경

--- runwithme/src/main/java/com/runwithme/runwithme/global/error/GlobalExceptionHandler.java
+++ runwithme/src/main/java/com/runwithme/runwithme/global/error/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.runwithme.runwithme.global.error;
 
 import com.runwithme.runwithme.global.error.exception.BusinessException;
+import com.runwithme.runwithme.global.result.ResultCode;
+import com.runwithme.runwithme.global.result.ResultResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,6 +10,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class })
+    protected ResponseEntity<ResultResponseDto> badParameter(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                ResultResponseDto.of(ResultCode.INVALID_PARAMETER_FAIL, e.getMessage())
+        );
+    }
+
     @ExceptionHandler
     protected ResponseEntity<ErrorResponseDto> handleBusinessException(BusinessException e) {
         final ErrorCode errorCode = e.getErrorCode();
