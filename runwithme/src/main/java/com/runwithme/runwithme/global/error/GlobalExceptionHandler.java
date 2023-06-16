54582fa33897b9da868a8ad74cee8b90e347a64d
Myeongbeom Lee <mungmnb777@gmail.com>
2023-06-17 03:06:54 +0900

    feat: Discord Webhook 로그 전송 기능 구현

--- runwithme/src/main/java/com/runwithme/runwithme/global/error/GlobalExceptionHandler.java
+++ runwithme/src/main/java/com/runwithme/runwithme/global/error/GlobalExceptionHandler.java
@@ -1,27 +1,52 @@
 package com.runwithme.runwithme.global.error;
 
-import com.runwithme.runwithme.global.error.exception.BusinessException;
 import com.runwithme.runwithme.global.result.ResultCode;
 import com.runwithme.runwithme.global.result.ResultResponseDto;
+import com.runwithme.runwithme.global.webhook.NotificationManager;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Enumeration;
+
+@Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
+    private final NotificationManager notificationManager;
+
     @ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class })
-    protected ResponseEntity<ResultResponseDto> badParameter(RuntimeException e) {
+    protected ResponseEntity<ResultResponseDto> badParameter(RuntimeException e, HttpServletRequest request) {
+        notificationManager.sendNotification(e, request.getRequestURI(), getParams(request));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
                 ResultResponseDto.of(ResultCode.INVALID_PARAMETER_FAIL, e.getMessage())
         );
     }
 
-    @ExceptionHandler
-    protected ResponseEntity<ErrorResponseDto> handleBusinessException(BusinessException e) {
-        final ErrorCode errorCode = e.getErrorCode();
-        final ErrorResponseDto response = ErrorResponseDto.of(errorCode);
-        return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ResultResponseDto> exception(Exception e, HttpServletRequest request) {
+        notificationManager.sendNotification(e, request.getRequestURI(), getParams(request));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                ResultResponseDto.of(ResultCode.INTERNAL_SERVER_ERROR, e.getMessage())
+        );
+    }
+
+    private String getParams(HttpServletRequest req) {
+        StringBuilder params = new StringBuilder();
+        Enumeration<String> keys = req.getParameterNames();
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            params.append(
+                    """
+                    - %s : %s
+                    """.formatted(key, req.getParameter(key))
+            );
+        }
+        return params.toString();
     }
 }
