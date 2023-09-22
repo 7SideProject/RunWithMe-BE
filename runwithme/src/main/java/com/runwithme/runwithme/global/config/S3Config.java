d0142622468b01565a268ecc872b5733c585a686
Myeongbeom Lee <mungmnb777@gmail.com>
2023-09-22 18:08:57 +0900

    refactor: AWS S3 -> NCP Object Storage 마이그레이션

--- runwithme/src/main/java/com/runwithme/runwithme/global/config/S3Config.java
+++ runwithme/src/main/java/com/runwithme/runwithme/global/config/S3Config.java
@@ -3,6 +3,7 @@ package com.runwithme.runwithme.global.config;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,13 +21,16 @@ public class S3Config {
     @Value("${cloud.aws.s3.region.static}")
     private String region;
 
+    @Value("${cloud.aws.s3.endpoint}")
+    private String endpoint;
+
     @Bean
     public AmazonS3 build() {
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
         return AmazonS3ClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(region)
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, region))
                 .build();
     }
 }
