5eb76d38480403162de1ae5d8761e57f4304051b
Myeongbeom Lee <mungmnb777@gmail.com>
2023-11-17 12:37:59 +0900

    refactor: iterator를 stream으로 리팩토링

--- runwithme/src/main/java/com/runwithme/runwithme/global/error/GlobalExceptionHandler.java
+++ runwithme/src/main/java/com/runwithme/runwithme/global/error/GlobalExceptionHandler.java
@@ -5,8 +5,10 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -51,10 +53,9 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-		Map<String, String> errors = new HashMap<>();
-		for (FieldError error : getFieldErrors(e)) {
-			errors.put(error.getField(), error.getDefaultMessage());
-		}
+		Map<String, String> errors = getFieldErrors(e).stream()
+			.collect(Collectors.toMap(FieldError::getField, err -> err.getDefaultMessage() == null ? "null" : err.getDefaultMessage()));
+
 		return ResponseEntity
 			.status(ResultCode.INVALID_PARAMETER_FAIL.getStatus())
 			.body(ResultResponseDto.of(ResultCode.INVALID_PARAMETER_FAIL, errors));
